112a09ba4f634e0c8293c80b6db595047cf25d72
Bernd Hahnebach <bernd@bimstatik.org>
2020-11-24 17:36:31 +0100

    code formatting

--- code_bimtester/run.py
+++ code_bimtester/run.py
@@ -1,33 +1,11 @@
-# ***************************************************************************
-# *   Copyright (c) 2020 Dion Moult <>                                      *
-# *   Copyright (c) 2020 Bernd Hahnebach <bernd@bimstatik.org>              *
-# *                                                                         *
-# *   This program is free software; you can redistribute it and/or modify  *
-# *   it under the terms of the GNU Lesser General Public License (LGPL)    *
-# *   as published by the Free Software Foundation; either version 2 of     *
-# *   the License, or (at your option) any later version.                   *
-# *   for detail see the LICENCE text file.                                 *
-# *                                                                         *
-# *   This program is distributed in the hope that it will be useful,       *
-# *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
-# *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
-# *   GNU Library General Public License for more details.                  *
-# *                                                                         *
-# *   You should have received a copy of the GNU Library General Public     *
-# *   License along with this program; if not, write to the Free Software   *
-# *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  *
-# *   USA                                                                   *
-# *                                                                         *
-# ***************************************************************************
-
-from behave.__main__ import main as behave_main
 import behave.formatter.pretty  # Needed for pyinstaller to package it
-import os
-import sys
-import shutil
-import webbrowser
 import fileinput
+import os
+import shutil
+import sys
 import tempfile
+import webbrowser
+from behave.__main__ import main as behave_main
 
 
 # get bimtester source code module path
